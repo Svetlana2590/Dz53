463a282128d5b27fc4e8c91994cf651b5d35d184
Medved <medved.odesa@gmail.com>
2024-11-12 12:54:04 -0300

    Commit 2

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/2')
+def hello_world2():  # put application's code here
+    return 'Hello222 World!'
+
 
 if __name__ == '__main__':
     app.run()
