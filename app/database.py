9dab1682a08078470083e3458e5c1d14f574208c
Medved <medved.odesa@gmail.com>
2024-11-14 14:36:05 -0300

    requirements

--- app/database.py
+++ app/database.py
@@ -6,4 +6,4 @@ class Config(object):
     # POSTS_PER_PAGE = 30
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-         'sqlite:///' + os.path.join(basedir, 'sqlite.db')
+         'sqlite:///' + os.path.join(basedir, 'python424.db')
