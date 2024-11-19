80cf92920de3362a8766790689036de2c9e5ae8a
Medved <medved.odesa@gmail.com>
2024-11-19 13:25:24 -0300

    db create table

--- app/main.py
+++ app/main.py
@@ -7,15 +7,10 @@ from flask_sqlalchemy import SQLAlchemy
 from database import Config
 from forms import LoginForm
 
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
-
 
 
 @app.route('/')
@@ -28,16 +23,11 @@ def login():
     form3 = LoginForm()
     if form3.validate_on_submit():
         from models import User
-        name=form3.username.data
-        print(form3.pasword.data)
-        print(form3.remember_me.data)
-        data=User(name=form3.username.data, password=form3.pasword.data, is_active=True)
+        name = form3.username.data
+        data = User(name=form3.username.data, password=form3.pasword.data, is_active=False)
         db.session.add(data)
         db.session.commit()
-
-        flash('YES,' + name + ' Flash flask work!!')
-        flash('YES, Flash flask work22!!')
-        flash('YES, Flash flask work3333!!')
+        flash('ПОЛЬЗОВАТЕЛЬ ' + name + ' ЗАРЕГИСТРИРОВАН')
         return redirect(url_for('index'))
     return render_template('login.html', form2=form3)
 
