0fd6bb06dd3474bc09c98676d03893beade848bc
Medved <medved.odesa@gmail.com>
2024-11-19 13:17:42 -0300

    db create table

--- app/main.py
+++ app/main.py
@@ -7,10 +7,15 @@ from flask_sqlalchemy import SQLAlchemy
 from database import Config
 from forms import LoginForm
 
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+
 
 
 @app.route('/')
@@ -22,9 +27,13 @@ def index():
 def login():
     form3 = LoginForm()
     if form3.validate_on_submit():
+        from models import User
         name=form3.username.data
         print(form3.pasword.data)
         print(form3.remember_me.data)
+        data=User(name=form3.username.data, password=form3.pasword.data, is_active=True)
+        db.session.add(data)
+        db.session.commit()
 
         flash('YES,' + name + ' Flash flask work!!')
         flash('YES, Flash flask work22!!')
--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 import sqlalchemy as sa
-import sqlalchemy.orm as orm
-from main import db
+from main import db, app
 
 
 class User(db.Model):
@@ -8,4 +7,8 @@ class User(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String(255), nullable=False, unique=True, index=True)
     password = sa.Column(sa.String(255), nullable=False)
+    is_active = sa.Column(sa.Boolean, default=True)
 
+
+with app.app_context():
+    db.create_all()
